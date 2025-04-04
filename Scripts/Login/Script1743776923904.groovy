0a8db9864e5879a419460bb33cf5915bf47b643b
Asani <Asani@DESKTOP-PBLC5NU>
2025-04-04 21:48:57 +0700

    add maximize windows

--- Scripts/Login/Script1743776923904.groovy
+++ Scripts/Login/Script1743776923904.groovy
@@ -19,6 +19,8 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
+WebUI.maximizeWindow()
+
 WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
 
 WebUI.click(findTestObject('Object Repository/Login_page/a_Make Appointment'))
